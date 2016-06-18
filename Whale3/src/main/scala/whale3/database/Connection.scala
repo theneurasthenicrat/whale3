6d2441670ed879debd2784eeccdc56eb29dee0d0
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2016-06-18 10:24:08 +0200

    Connection uses default data source

--- Whale3/src/main/scala/whale3/database/Connection.scala
+++ Whale3/src/main/scala/whale3/database/Connection.scala
@@ -3,30 +3,31 @@ package whale3.database
 import java.sql.SQLException
 import java.sql.Statement
 import javax.sql.DataSource
+import javax.annotation.Resource
 
 object Connection {
-	//  @Resource(name = "jdbc/whale3")
-	private val ds: DataSource = new javax.naming.InitialContext().lookup("java:comp/env/jdbc/whale3").asInstanceOf[DataSource]
-	private var connec: java.sql.Connection = null
+  @Resource
+  private val ds: DataSource = null
+  private var connec: java.sql.Connection = null
 
-	def getConnection(): java.sql.Connection = {
-		try {
-			if (connec == null) {
-				connec = ds.getConnection()
-				connec.setAutoCommit(false)
-			}
-			connec
-		} catch {
-			case ex: SQLException => throw new DataBaseException("Database error while trying to get a new connection. Error information: " + ex.toString())
-		}
-	}
+  def getConnection(): java.sql.Connection = {
+    try {
+      if (connec == null) {
+        connec = ds.getConnection()
+        connec.setAutoCommit(false)
+      }
+      connec
+    } catch {
+      case ex: SQLException => throw new DataBaseException("Database error while trying to get a new connection. Error information: " + ex.toString())
+    }
+  }
 
-	def closeConnection(): Unit = {
-		try {
-			if (connec != null) connec.close()
-		} catch {
-			case ex: SQLException => throw new DataBaseException("Database error while trying to close the connection. Error information: " + ex.toString())
-		}
-	}
+  def closeConnection(): Unit = {
+    try {
+      if (connec != null) connec.close()
+    } catch {
+      case ex: SQLException => throw new DataBaseException("Database error while trying to close the connection. Error information: " + ex.toString())
+    }
+  }
 }
   
